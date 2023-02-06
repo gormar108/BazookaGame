e3611b79a9300d5bc5c3662560a47dcfbdbf7457
Voodles3 <voodles10@gmail.com>
2023-02-06 14:06:48 -0800

    Added Jumping, Drag, and Speed Control
    Basic jumping and movement restraints

--- Bazooka Game/Assets/Scripts/Movement.cs
+++ Bazooka Game/Assets/Scripts/Movement.cs
@@ -16,15 +16,36 @@ public class Movement : MonoBehaviour
 
     Rigidbody rb;
 
+    public float playerHeight;
+    public LayerMask thisIsGround;
+    bool grounded;
+    public float groundDrag;
+    public float airDrag;
+
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        rb.freezeRotation = true;
+        readyToJump = true;
     }
 
     void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, thisIsGround);
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
         GetInput();
+        SpeedControl();
     }
 
     void FixedUpdate()
@@ -36,12 +57,46 @@ public class Movement : MonoBehaviour
     {
         horizInput = Input.GetAxisRaw("Horizontal");
         vertInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     void MovePlayer()
     {
         moveDirection = playerBody.forward * vertInput + playerBody.right * horizInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 maxVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(maxVel.x, rb.velocity.y, maxVel.z);
+        }
+    }
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+    }
+    void ResetJump()
+    {
+        readyToJump = true;
     }
 }
