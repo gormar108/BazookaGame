1db4a0a4cc7df4d739cdf1e085ba64b494cc697b
gormar108 <gormar108@gmail.com>
2023-04-11 12:00:17 +0200

    Made the collider on player in the same place as rigidbody(explosion knocks player back now)
    you can barrel jump

--- Bazooka Game/Assets/Scripts/ExplosionScript.cs
+++ Bazooka Game/Assets/Scripts/ExplosionScript.cs
@@ -32,7 +32,7 @@ public class ExplosionScript : MonoBehaviour
     }
     IEnumerator Despawn()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.7f);
         Destroy(gameObject);
     }
 }
