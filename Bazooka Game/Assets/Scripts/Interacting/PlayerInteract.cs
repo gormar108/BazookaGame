315ee1309cb0ad53e5fe3b7c6393508267e92701
gormar108 <gormar108@gmail.com>
2023-04-20 19:18:24 +0200

    Made improvements on interaction system

--- Bazooka Game/Assets/Scripts/Interacting/PlayerInteract.cs
+++ Bazooka Game/Assets/Scripts/Interacting/PlayerInteract.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteract : MonoBehaviour
 {
     private Camera cam;
     [SerializeField] private float distance = 5f; 
     [SerializeField] private LayerMask mask;
+    [SerializeField] GameObject canvasText;
+    [SerializeField] TextMeshProUGUI textlabel;
+
     void Start()
     {
         cam = GetComponent<Movement>().PlayerCam;
+
     }
 
     // Update is called once per frame
@@ -23,11 +28,17 @@ public class PlayerInteract : MonoBehaviour
         {
             if(hitinfo.collider.GetComponent<Interactable>() != null)
             {
-                Debug.Log(hitinfo.collider.GetComponent<Interactable>().promptMessage);
                 
+                Debug.Log(hitinfo.collider.GetComponent<Interactable>().promptMessage);
+                canvasText.active = true;
+                textlabel.text = hitinfo.collider.GetComponent<Interactable>().promptMessage;
 
             }
 
+        }
+        else
+        {
+            canvasText.active = false;
         }
     }
 }
